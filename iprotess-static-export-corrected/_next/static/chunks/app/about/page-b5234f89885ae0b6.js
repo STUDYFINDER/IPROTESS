20ba578991c4a41650730dba0f12e7a061cc66ac
STUDYFINDER <146574502+STUDYFINDER@users.noreply.github.com>
2025-05-11 21:10:32 +0200

    Add files via upload

---
+++ iprotess-static-export-corrected/_next/static/chunks/app/about/page-b5234f89885ae0b6.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[220],{1296:e=>{e.exports={style:{fontFamily:"'Inter', 'Inter Fallback'"},className:"__className_d7ceed"}},2190:(e,s,a)=>{Promise.resolve().then(a.t.bind(a,1296,23))}},e=>{var s=s=>e(e.s=s);e.O(0,[77,441,684,358],()=>s(2190)),_N_E=e.O()}]);
